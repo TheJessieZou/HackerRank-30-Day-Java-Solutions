2df99f3db8425247f93f9a452ad8eb6528da1db9
Jessie Zou <jessiehaozou@gmail.com>
2018-04-01 12:34:52 -0700

    Operators

---
+++ day2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double meal_cost = in.nextDouble();
+        int tip_percent = in.nextInt();
+        int tax_percent = in.nextInt();
+        in.close();
+        
+        //must divide by 100.0 to makes sure that integers are evaluated as doubles
+        double tip = meal_cost * (tip_percent / 100.0);
+        double tax = meal_cost * (tax_percent / 100.0);
+        
+        double total_cost = meal_cost + tip + tax;
+        System.out.println("The total meal cost is " + Math.round(total_cost) + " dollars.");
+    }
+}
