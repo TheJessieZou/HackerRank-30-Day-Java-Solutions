899f214ed19bec163139a37d89955337a99e83b3
Jessie Zou <jessiehaozou@gmail.com>
2018-04-01 12:15:39 -0700

    day1

---
+++ day1.java
@@ -0,0 +1,20 @@
+        /* Declare second integer, double, and String variables. */
+        int j;
+        double e;
+        String dog;
+    
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        j = scan.nextInt();
+        e = scan.nextDouble();
+        scan.nextLine(); //required to scan the entire line
+        dog = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + j);
+
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d + e);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s + dog);
