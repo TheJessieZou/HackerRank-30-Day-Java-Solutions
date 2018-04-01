b667748166a5215a0e1046ce5acfd9ebad5d8e13
Jessie Zou <jessiehaozou@gmail.com>
2018-04-01 13:20:55 -0700

    Intro to Conditionals

--- day3.java
+++ day3.java
@@ -10,7 +10,8 @@ public class Solution {
       int n = scan.nextInt(); 
       scan.close();
       String ans="";
-          
+      
+      //gucci gang
       // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
       if(n%2==1 || (n>5 && n<21)){
          ans = "Weird";
